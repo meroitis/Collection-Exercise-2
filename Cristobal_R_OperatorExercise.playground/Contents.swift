ba68fc51bc852f265c773d95b9ff391fb616f01d
meroitis <67976213+meroitis@users.noreply.github.com>
2020-07-16 15:04:37 -0500

    Add files via upload

---
+++ Cristobal_R_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// Inputs
+var a = 10
+var b = 20
+ 
+// Operations
+a + b
+a = 30
+b = 60
+a * b
+a = 5
+b = 80
+b / a
+
+// Prints with last variables 
+print(a)
+print(b)
+print(a + b)
+print (a * b)
+print (b / a)
+
+
+
